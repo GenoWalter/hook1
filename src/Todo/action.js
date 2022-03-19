ccdadbff40e62c8d257a63ac8d48785ab41fd67c
Your Name <you@example.com>
2022-03-19 22:28:02 +0700

    add

---
+++ src/Todo/action.js
@@ -0,0 +1,21 @@
+import { SET_J0B, ADD_JOB, DELETE_JOB } from './constants'
+
+export const setJob = payload => {
+    return {
+        type: SET JOB,
+        payload
+    }
+}export 
+
+export const addJob = payload => {
+    return {
+        type: ADD JOB,
+        payload
+    }
+}
+export const deleteJob = payload => {
+    return {
+        type: DELETE_JOB,
+        payload
+    }
+}
